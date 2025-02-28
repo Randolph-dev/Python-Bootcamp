0487b79f6b8049c6924c9771bef4e7281a01262e
Randolph-dev <randolph2894@gmail.com>
2025-02-28 11:54:05 +0200

    add while loops lessons

---
+++ While_loops/main.py
@@ -0,0 +1,31 @@
+# while loop = exectue some code WHILE some condition remains true
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# food = input("Enter a food you like (q to quit): ")
+num = int(input("Enter a number between 1 - 10: "))
+
+# while name == "":
+#     print("You did not enter your name")
+#     name = input("Enter your name: ")
+
+# else: 
+#     print(f"Hello {name}")
+
+# while age < 0:
+#     print("Age can't be negative")
+#     age = int(input("Enter your age: "))
+
+# print(f"You are {age} years old")
+
+# while not food == "q":
+#     print(f"You like {food}")
+#     food = input("Enter another food you like (q to quit): ")
+
+# print("bye")
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a # between 1 - 10: "))
+
+print(f"Your number is {num}")
