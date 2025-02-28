2c9b9e28434860ba5adbbc8f76389040bfb5318b
Randolph-dev <randolph2894@gmail.com>
2025-02-28 12:54:59 +0200

    add variable collections practice exercise

---
+++ Collections/main.py
@@ -0,0 +1,30 @@
+# collection = single "variable" used to store multiple values
+#   List = [] ordered and changeable. Duplicates OK
+#   Set = {} unordered and immutable, but Add/Remove OK. NO duplicates
+#   Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+
+fruits = {"apple", "orange", "banana", "coconut"}
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("apple" in fruits)
+
+# print(fruits[::-1])
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("apple"))
+# print(fruits.count("banana"))
+# fruits.pop
+
+print(fruits)
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+# print
