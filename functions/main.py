3595f8802fbdeb439d0b04e235438491a9706188
Randolph-dev <randolph2894@gmail.com>
2025-03-05 23:44:49 +0200

    add simple function exercises in python

---
+++ functions/main.py
@@ -0,0 +1,40 @@
+# -------> function = a block of reusable code
+#            place () after the function name to invoke it
+
+# def happy_birthday(name, age):
+#     print(f"Happy birthday to {name}!")
+#     print(f"You are {age} years old!")
+#     print("Happy birthday to you!")
+#     print()
+
+# happy_birthday("Myself", 20)
+# happy_birthday("Joe", 30)
+# happy_birthday("Steve", 40)
+
+# def display_invoice(username, amount, due_date):
+#     print(f"Hello {username}")
+#     print(f"Your bill of ${amount:.2f} is due {due_date}")
+
+# display_invoice("Joe", 100.50, "01/02")
+
+# -------> return = statement used to end a function
+#                   and send a result back to the caller
+
+# def add(x, y):
+#     z = x + y
+#     return z
+
+# def substract(x, y):
+#     z = x - y
+#     return z
+# def multiply(x, y):
+#     z = x * y
+#     return z
+# def divide(x, y):
+#     z = x / y
+#     return z
+
+# print(add(1, 2))
+# print(substract(1, 2))
+# print(multiply(3, 3))
+# print(divide(3, 3))
